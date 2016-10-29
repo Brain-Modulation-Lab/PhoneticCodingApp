7a8603455e11c1f7c197d1a56847a3a37068e3b6
Tara Pirnia <tara.pirnia@gmail.com>
2016-10-29 14:49:18 -0400

    Phonetic Coding app version 1.2. Updated to log each time files are edited with which app version. Also added LoadAllTrials.m to segment all trials with 1s before onset and .5s after end marker when data file is opened rather than each time a trial is changed.

---
+++ LoadAllTrials.m
@@ -0,0 +1,19 @@
+function [numTrials, AudioTrials] =LoadAllTrials(AudioData) 
+numTrials=floor((length(AudioData.EventTimes)-AudioData.SkipEvents)/4);
+EventTimes1 = [AudioData.EventTimes AudioData.EventTimes(end)+2];
+for i=1:numTrials
+    
+    StimulusEvent1 = AudioData.SkipEvents + 4*i;
+    StimulusEvent2 = AudioData.SkipEvents + 4*i + 1;
+   
+    sf=30000;
+    
+    CurrAudioTrial = AudioData.Audio(round(sf*(EventTimes1(StimulusEvent1)-1)): ...
+    round(sf*(EventTimes1(StimulusEvent2)+.5)));
+
+
+    AudioTrials(:,i) = {(2*(CurrAudioTrial - mean(CurrAudioTrial))/ ...
+    (max(CurrAudioTrial) - min(CurrAudioTrial)))};
+    
+end
+
